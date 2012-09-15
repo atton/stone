ff801ea31bd8e91a64492c5974ec343a82591441
atton <e115763@gmail.com>
2012-09-15 20:19:31 +0900

    fix block

--- ScriptLanguage_Stone/src/chap7/FuncEvaluator.java
+++ ScriptLanguage_Stone/src/chap7/FuncEvaluator.java
@@ -39,33 +39,33 @@ import chap6.BasicEvaluator.BlockEx;
 			} else
 				return ((ASTreeEx)operand()).eval(env);
 		}
-		@Reviser public static abstract class PostfixEx extends Postfix {
-			public PostfixEx(List<ASTree> c) { super(c); }
-			public abstract Object eval(Environment env, Object value);
-			
+	}
+	@Reviser public static abstract class PostfixEx extends Postfix {
+		public PostfixEx(List<ASTree> c) { super(c); }
+		public abstract Object eval(Environment env, Object value);
+
+	}
+	@Reviser public static class ArgumentsEx extends Arguments {
+		public ArgumentsEx(List<ASTree> c) { super(c); }
+		public Object eval(Environment callerEnv, Object value) {
+			if (!(value instanceof Function))
+				throw new StoneException("bad function",this);
+			Function func = (Function)value;
+			ParameterList params = func.parameters();
+			if (size() != params.size())
+				throw new StoneException("bad number of arguments", this);
+			Environment newEnv = func.makeEnv();
+			int num = 0;
+			for(ASTree a: this)
+				((ParamsEx)params).eval(newEnv,num++,
+						((ASTreeEx)a).eval(callerEnv));
+			return ((BlockEx)func.body()).eval(newEnv);
 		}
-		@Reviser public static class ArgumentsEx extends Arguments {
-			public ArgumentsEx(List<ASTree> c) { super(c); }
-			public Object eval(Environment callerEnv, Object value) {
-				if (!(value instanceof Function))
-					throw new StoneException("bad function",this);
-				Function func = (Function)value;
-				ParameterList params = func.parameters();
-				if (size() != params.size())
-					throw new StoneException("bad number of arguments", this);
-				Environment newEnv = func.makeEnv();
-				int num = 0;
-				for(ASTree a: this)
-					((ParamsEx)params).eval(newEnv,num++,
-							((ASTreeEx)a).eval(callerEnv));
-				return ((BlockEx)func.body()).eval(newEnv);
-			}
-		}
-		@Reviser public static class ParamsEx extends ParameterList {
-			public ParamsEx(List<ASTree> c) { super(c); }
-			public void eval(Environment env, int index, Object value) {
-				((EnvEx)env).putNew(name(index), value);
-			}
+	}
+	@Reviser public static class ParamsEx extends ParameterList {
+		public ParamsEx(List<ASTree> c) { super(c); }
+		public void eval(Environment env, int index, Object value) {
+			((EnvEx)env).putNew(name(index), value);
 		}
 	}
 }
