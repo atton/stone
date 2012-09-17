93406c5fc107052b56a60a2efbf902022d4dec89
atton <e115763@gmail.com>
2012-09-17 10:59:31 +0900

    fix ClassParser

--- ScriptLanguage_Stone/src/stone/ClassParser.java
+++ ScriptLanguage_Stone/src/stone/ClassParser.java
@@ -7,7 +7,7 @@ import stone.ast.Dot;
 public class ClassParser extends ClosureParser {
 	Parser member = rule().or(def, simple);
 	Parser class_body = rule(ClassBody.class).sep("{").option(member)
-			.repeat(rule().sep(";", Token.EOL).option(member).sep("}"));
+			.repeat(rule().sep(";", Token.EOL).option(member)).sep("}");
 	Parser defclass = rule(ClassStmnt.class).sep("class").identifier(reserved)
 			.option(rule().sep("extends").identifier(reserved)).ast(class_body);
 	public ClassParser() {
