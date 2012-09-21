27b4f6d569545de6652e68c0b5f0a9eb66fef270
atton <e115763@gmail.com>
2012-09-21 13:42:37 +0900

    wrote chap13 to 13.3

---
+++ ScriptLanguage_Stone/src/chap13/StoneVMEnv.java
@@ -0,0 +1,16 @@
+package chap13;
+import chap11.ResizableArrayEnv;
+
+public class StoneVMEnv extends ResizableArrayEnv implements HeapMemory {
+	protected StoneVM svm;
+	protected Code code;
+	public StoneVMEnv(int codeSize, int stackSize, int stringSize) {
+		svm = new StoneVM(codeSize, stackSize, stringSize, this);
+		code = new Code(svm);
+	}
+	public StoneVM stoneVM() { return svm; }
+	public Code code() { return code; }
+	public Object read(int index) { return values[index]; }
+	public void write(int index, Object v) { values[index] = v; }
+
+}
